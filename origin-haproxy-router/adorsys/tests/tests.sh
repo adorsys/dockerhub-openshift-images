61d8c257e2d4bb014a7f3ef486804fda47c5b685
Jan-Otto Kröpke <mail@jkroepke.de>
2020-04-11 16:22:49 +0200

    Refactor patches

--- origin-haproxy-router/adorsys/tests/tests.sh
+++ origin-haproxy-router/adorsys/tests/tests.sh
@@ -3,3 +3,5 @@
 docker run --rm --entrypoint=/usr/sbin/haproxy "${DOCKER_IMAGE}:${TAG}" -vv
 docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -v | grep '2.0.14'"
 docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -vv | grep 'TLSv1.3'"
+
+"$(git rev-parse --show-toplevel)"/origin-haproxy-router/adorsys/contrib/test-patches.sh
