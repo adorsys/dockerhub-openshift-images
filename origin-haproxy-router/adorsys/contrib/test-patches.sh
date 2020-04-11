d010f2de42a39a4e2fb95ca3207b689d1024844a
Jan-Otto Kröpke <mail@jkroepke.de>
2020-04-11 18:37:07 +0200

    Remove TTY

--- origin-haproxy-router/adorsys/contrib/test-patches.sh
+++ origin-haproxy-router/adorsys/contrib/test-patches.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -u0 --entrypoint=/bin/bash -v "$(git rev-parse --show-toplevel)/origin-haproxy-router/adorsys/patches:/patches" --rm -ti openshift/origin-haproxy-router:v3.11.0 \
+docker run -u0 --entrypoint=/bin/bash -v "$(git rev-parse --show-toplevel)/origin-haproxy-router/adorsys/patches:/patches" --rm openshift/origin-haproxy-router:v3.11.0 \
   -c "set -e; yum install -q -y patch; for patch in /patches/*.patch; do echo \$patch; patch -u -l -f /var/lib/haproxy/conf/haproxy-config.template \"\${patch}\"; done"
