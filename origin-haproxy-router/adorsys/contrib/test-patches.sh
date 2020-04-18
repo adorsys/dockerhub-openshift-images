0fabf3e2dc3aaef30271320688a777f8d8e05919
Jan-Otto Kröpke <mail@jkroepke.de>
2020-04-18 14:16:23 +0200

    Disable TLSv1.3 at backend

--- origin-haproxy-router/adorsys/contrib/test-patches.sh
+++ origin-haproxy-router/adorsys/contrib/test-patches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker pull openshift/origin-haproxy-router:v3.11.0
 docker run -u0 --entrypoint=/bin/bash -v "$(git rev-parse --show-toplevel)/origin-haproxy-router/adorsys/patches:/patches" --rm openshift/origin-haproxy-router:v3.11.0 \
   -c "set -e; yum install -q -y patch; for patch in /patches/*.patch; do echo \$patch; patch -u -l -f /var/lib/haproxy/conf/haproxy-config.template \"\${patch}\"; done"
