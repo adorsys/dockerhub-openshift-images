78d9d3d1b48c02245ecac67318128fb24d1d8de8
Jan-Otto Kröpke <mail@jkroepke.de>
2020-04-18 14:24:51 +0200

    Fixed tests

--- origin-haproxy-router/adorsys/tests/tests.sh
+++ origin-haproxy-router/adorsys/tests/tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+HAPROXY_VERSION=$(grep HAPROXY_PATCH_VERSION= "$(git rev-parse --show-toplevel)"/origin-haproxy-router/adorsys/Dockerfile | cut -d= -f2)
+
 docker run --rm --entrypoint=/usr/sbin/haproxy "${DOCKER_IMAGE}:${TAG}" -vv
-docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -v | grep '2.0.14'"
+docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -v | grep '${HAPROXY_VERSION}'"
 docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -vv | grep 'TLSv1.3'"
 
 "$(git rev-parse --show-toplevel)"/origin-haproxy-router/adorsys/contrib/test-patches.sh
