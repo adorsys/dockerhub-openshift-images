893b747338dda7954c554a3a6a573190f5492819
Jan-Otto Kröpke <joe@adorsys.de>
2020-06-07 23:16:55 +0200

    bump

---
+++ contrib/update-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sedi() {
+  if [ "$(uname)" == "Darwin" ]; then
+    sed -i "" "$@"
+  else
+    sed -i "$@"
+  fi
+}
+
+RH_FTP_OUTPUT="$(curl -sSf https://ftp.redhat.com/redhat/linux/enterprise/7Server/en/RHOSE/SRPMS/)"
+
+REGISTRY_VERSION=$(echo "${RH_FTP_OUTPUT}" | grep -o -E 'atomic-openshift-dockerregistry.*.rpm"' | cut -d- -f4-5 | sed -e 's/.el7.src.rpm"//g' | tail -n1)
+sedi "s/ARG RPM_VERSION=.*/ARG RPM_VERSION=${REGISTRY_VERSION}/" origin-docker-registry/Dockerfile
+docker build -t adorsys/origin-docker-registry:v3.11 origin-docker-registry
+
+WEB_CONSOLE_VERSION=$(echo "${RH_FTP_OUTPUT}" | grep -o -E 'atomic-openshift-web-console.*.rpm"' | cut -d- -f5-6 | sed -e 's/.el7.src.rpm"//g' | tail -n1)
+sedi "s/ARG RPM_VERSION=.*/ARG RPM_VERSION=${WEB_CONSOLE_VERSION}/" origin-web-console/Dockerfile
+docker build -t adorsys/origin-web-console:v3.11 origin-web-console
