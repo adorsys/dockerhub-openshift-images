ed57e8bc3f333c58990e125d57a2286d71453fbf
Jan-Otto Kröpke <mail@jkroepke.de>
2020-08-29 16:07:14 +0200

    Do not test images

--- contrib/update-images.sh
+++ contrib/update-images.sh
@@ -15,6 +15,3 @@ sedi "s/ARG RPM_VERSION=.*/ARG RPM_VERSION=${REGISTRY_VERSION}/" origin-docker-r
 
 WEB_CONSOLE_VERSION=$(echo "${RH_FTP_OUTPUT}" | grep -o -E 'atomic-openshift-web-console.*.rpm"' | cut -d- -f5-6 | sed -e 's/.el7.src.rpm"//g' | tail -n1)
 sedi "s/ARG RPM_VERSION=.*/ARG RPM_VERSION=${WEB_CONSOLE_VERSION}/" origin-web-console/Dockerfile
-
-docker build -t adorsys/origin-docker-registry:v3.11 origin-docker-registry
-docker build -t adorsys/origin-web-console:v3.11 origin-web-console
