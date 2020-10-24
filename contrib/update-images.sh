012ca9dfd63ae6edfb72424e1f598c1998ed6a5c
Jan-Otto Kröpke <joe@adorsys.de>
2020-10-24 16:18:01 +0200

    update haproxy

--- contrib/update-images.sh
+++ contrib/update-images.sh
@@ -20,3 +20,9 @@ sedi "s/ARG RPM_VERSION=.*/ARG RPM_VERSION=${WEB_CONSOLE_VERSION}/" origin-web-c
 
 docker build -t adorsys/origin-docker-registry:v3.11 origin-docker-registry
 docker build -t adorsys/origin-web-console:v3.11 origin-web-console
+
+HAPROXY_VERSION=$(curl -sSfL http://www.haproxy.org/download/2.2/src/ | grep -o -e 'haproxy-2\.[0-9]\.[0-9]*\.tar\.gz' | sed -E 's/haproxy-(.+)\.tar\.gz/\1/g' | uniq | sort -k 3 -t . -n -r | head -n1)
+
+if [ "${HAPROXY_VERSION}" != "" ]; then
+  sedi "s/HAPROXY_PATCH_VERSION=.*/HAPROXY_PATCH_VERSION=${HAPROXY_VERSION}/" origin-haproxy-router/adorsys/Dockerfile
+fi
