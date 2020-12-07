6a682938354c1099be9f312459669ff5887afa6c
Jan-Otto Kröpke <joe@adorsys.de>
2020-12-07 01:12:32 +0100

    fixed ip4 forwarded

--- origin-haproxy-router/adorsys/contrib/dl-config-template.sh
+++ origin-haproxy-router/adorsys/contrib/dl-config-template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --entrypoint=/bin/cat --rm -ti openshift/origin-haproxy-router:v3.11.0 /var/lib/haproxy/conf/haproxy-config.template > haproxy-config.template
+docker run --entrypoint=/bin/cat --rm -ti adorsys/origin-haproxy-router:v3.11.0-adorsys /var/lib/haproxy/conf/haproxy-config-adorsys.template > haproxy-config.template
