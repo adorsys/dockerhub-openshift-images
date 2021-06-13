cac7139c01105559210039aa7dcb306a748de372
Jan-Otto Kröpke <joe@adorsys.de>
2021-06-13 22:48:57 +0200

    fixed tests

--- origin-haproxy-router/original/tests/tests.sh
+++ origin-haproxy-router/original/tests/tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker run --rm --entrypoint=/usr/sbin/haproxy "${DOCKER_IMAGE}:${TAG}" -vv
-docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -v | grep '1.8.26'"
+docker run --rm --entrypoint=/bin/bash "${DOCKER_IMAGE}:${TAG}" -c "haproxy -v | grep '1.8.'"
